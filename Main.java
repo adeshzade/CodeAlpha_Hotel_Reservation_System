62d4347313a47e2cb0c9852b579c048287b272b0
Adesh Zade <adeshzade7@gmail.com>
2026-07-15 12:34:53 +0530

    Add Main class to start hotel reservation system
    A Hotel Management System (HMS) is a software platform designed to automate and centralize a lodging property's daily operations. It acts as a digital "command center" that synchronizes reservations, front desk tasks, housekeeping, billing, and inventory from a single dashboard, replacing traditional spreadsheets and paper logs.

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+        hotelReservationSystem.start();
+
+    }
+
+}
